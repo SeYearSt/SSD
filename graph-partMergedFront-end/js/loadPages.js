8b1bb5b8635e2cc0092c2007034b816d120e471d
oleh <jtiks06@gmail.com>
2017-11-29 01:04:11 +0200

    Added check for permission while page loading

--- graph-partMergedFront-end/js/loadPages.js
+++ graph-partMergedFront-end/js/loadPages.js
@@ -4,15 +4,26 @@ $( document ).ready( function (){
 		url:"https://someleltest.herokuapp.com/api/users/checkPermision?SID="+JSON.parse(localStorage.getItem("SID")),
 		success:function(response){console.log(response);}
 	})
-	/*var permision=localStorage.getItem("SID");
-	if(!localStorage.getItem("permission")){location.replace("index.html");}*/
-	var fileName=location.pathname.match(/[^\/]+$/)[0];
+	})
 	var permission=JSON.parse(localStorage.getItem("permission"));
-	console.log(permission);
-})
-pages={
-	"admin":["index.html","log-in.html","order.html","planets.html","register_order.html","ships.html","sing-up.html","tfa.html","users.html"],
-	"operator":["index.html","log-in.html","order.html","planets.html","register_order.html","ships.html","sing-up.html","tfa.html"],
+	permission=permission? permission:"unauthorized";
+
+var pages={
+	admin:["index.html","log-in.html","order.html","planets.html","register_order.html","ships.html","sing-up.html","tfa.html","users.html"],
+	operator:["index.html","log-in.html","order.html","planets.html","register_order.html","ships.html","sing-up.html","tfa.html"],
 	default:["index.html","log-in.html","order.html","planets.html","register_order.html","sing-up.html","tfa.html"],
 	unauthorized:["index.html","log-in.html","order.html","planets.html","register_order.html","sing-up.html"]
 }
+function loadPageByPerm(perm){
+	console.log(perm);
+	console.log(typeof(perm));
+	var fileName=location.pathname.match(/[^\/]+$/)[0];
+	var accessPage=pages[perm];
+	console.log(accessPage);
+	var pr=false;
+	$.each(accessPage, function( index, value ) {
+ 	if (fileName===value) pr=true;
+	})
+	if (!pr){window.stop();location.replace("index.html")};
+}
+loadPageByPerm(permission);
